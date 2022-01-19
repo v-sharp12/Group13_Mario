7b1613b4054e878a2b4bdc93b25fab32a3abf8b0
v-sharp12 <vmjohnson955@gmail.com>
2022-01-19 14:20:00 -0500

    built

--- Mario_Project/Assets/Scripts/gameManager.cs
+++ Mario_Project/Assets/Scripts/gameManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 
